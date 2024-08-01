25cbbdf088fdf1a8adfaf1cce6fc5da2486c56e0
tky-san <cist.b214.t.ito@gmail.com>
2024-08-02 00:04:23 +0900

    AOPサンプルプロジェクト完成

--- AOPSample/src/main/java/com/example/demo/AopSampleApplication.java
+++ AOPSample/src/main/java/com/example/demo/AopSampleApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.service.TargetService;
 
 @SpringBootApplication
 public class AopSampleApplication {
 
+	/** 起動 */
 	public static void main(String[] args) {
-		SpringApplication.run(AopSampleApplication.class, args);
+		SpringApplication.run(AopSampleApplication.class, args).getBean(AopSampleApplication.class).exe();
+	}
+	
+	/** DI */
+	@Autowired
+	private TargetService service;
+
+	/** 実行 */
+	private void exe() {
+		service.sayHello("太郎");
+		// 分かりやすいように区切りを表示
+		System.out.println("■□■□■□■□■□");
+		service.sayGoodbye("花子");
 	}
 
 }
--- AOPSample/src/main/java/com/example/demo/aop/LoggingAspect.java
+++ AOPSample/src/main/java/com/example/demo/aop/LoggingAspect.java
@@ -1,5 +1,11 @@
 package com.example.demo.aop;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
@@ -7,4 +13,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 	
+	// @Before("execution(* com.example.demo.service.TargetService.*(..))")
+	public void beforeAdvice(JoinPoint joinPoint) {
+		LocalDateTime startTime = LocalDateTime.now(); //現在の日時を取得
+		String formattedTime = startTime.format(DateTimeFormatter.ofPattern("HH:mm:ss:SSS"));
+		System.out.println("-------【@Before】-------");
+		System.out.println("Before method : " + joinPoint.getSignature());
+		System.out.println("メソッド開始 : " + formattedTime);
+	}
+	
+	// @After("execution(* com.example.demo.service.TargetService.*(..))")
+	public void afterAdvice(JoinPoint joinPoint) {
+		LocalDateTime endTime = LocalDateTime.now(); //現在の日時を取得
+		String formattedTime = endTime.format(DateTimeFormatter.ofPattern("HH:mm:ss:SSS"));
+		System.out.println("-------【@After】-------");
+		System.out.println("After method : " + joinPoint.getSignature());
+		System.out.println("メソッド開始 : " + formattedTime);
+	}
+	
+	@Around("execution(* com.example.demo.service.TargetService.*(..))")
+	public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		System.out.println("=====【@Around : 前】=====");
+		System.out.println("■Target");
+		System.out.println(" クラス : " + joinPoint.getSignature().getDeclaringTypeName());
+		System.out.println(" メソッド : " + joinPoint.getSignature().getName());
+		
+		Object result = joinPoint.proceed(); // 実行メソッドを呼び出す
+		
+		System.out.println("=====【@Around : 後】=====");
+		long elapsedTime = System.currentTimeMillis() - startTime;
+		System.out.println("Method executio time: " + elapsedTime + "milliseconds.");
+		return result;
+	}
 }
