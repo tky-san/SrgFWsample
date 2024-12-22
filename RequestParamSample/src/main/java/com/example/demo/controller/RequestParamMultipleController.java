59be4737221ab723df430c00226da00362a5d016
tky-san <cist.b214.t.ito@gmail.com>
2024-12-22 17:07:26 +0900

    7-2実装完了

--- RequestParamSample/src/main/java/com/example/demo/controller/RequestParamMultipleController.java
+++ RequestParamSample/src/main/java/com/example/demo/controller/RequestParamMultipleController.java
@@ -1,13 +1,10 @@
 package com.example.demo.controller;
 
-import java.time.LocalDate;
-
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.form.SampleForm;
 
 @Controller
 public class RequestParamMultipleController {
@@ -20,16 +17,23 @@ public class RequestParamMultipleController {
 	}
 	
 	// POSTかつ [url:/confirm]
+//	@PostMapping("confirm")
+//	public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age, 
+//			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
+//		
+//		// Modelに格納する
+//		model.addAttribute("name", name);
+//		model.addAttribute("age", age);
+//		model.addAttribute("birth", birth);
+//		
+//		// 戻り値は「ビュー名」を返す
+//		return "confirm";
+//	}
+	
+	// POSTかつ [url:/confirm]
 	@PostMapping("confirm")
-	public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age, 
-			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
-		
-		// Modelに格納する
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
-		model.addAttribute("birth", birth);
-		
+	public String confirmView(SampleForm f) {
 		// 戻り値は「ビュー名」を返す
-		return "confirm";
+		return "confirm2";
 	}
 }
